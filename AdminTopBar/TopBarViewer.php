5ab63f7848fd39048f57e9c7e140cd483034c0ed
Jona <jona303@gmail.com>
2016-04-22 15:39:41 +0200

    new nav bar

--- AdminTopBar/TopBarViewer.php
+++ AdminTopBar/TopBarViewer.php
@@ -7,69 +7,70 @@ use Symfony\Component\HttpFoundation\Response;
  *
  * @author Jona
  */
-class TopBarViewer 
+class TopBarViewer
 {
     private $twig;
-    
+
     private $container;
-    
-    
+
+
     public function __construct($container)
     {
         $this->container = $container;
         $this->twig= $container->get('twig');
-        
+
     }
-    
-    
+
+
     public function displayAdminTopBar(Response $response,$editPath)
     {
         $adminTopBar = $this->getAdminTopBar($editPath);
         $htmlContent = $response->getContent();
-        
+
 //        die($adminTopBar);
 //        die();
-        
+
 //        if (!preg_match('#col-md#',$htmlContent)) //Bootsrap Fix
 //        {
-            $bootstrap = $this->getBootsrap();
-            $htmlContent = preg_replace('#(<head>)#','$1'.$bootstrap,$htmlContent);
-            
+        $bootstrap = $this->getBootsrap();
+        $htmlContent = preg_replace('#(<head [ a-zA-Z0-9\"\'=_-]*>)#','$1'.$bootstrap,$htmlContent);
+
 //            $bootsrapJS = '<script type="text/javascript" src="'.$this->container->get('templating.helper.assets')->getUrl('bundles/skcmsadmin/js/bootstrap.min.js').'"></script>';
-            $bootsrapJS = '<script type="text/javascript" src="'.$this->container->get('templating.helper.assets')->getUrl('bundles/skcmsadmin/js/topBar.js').'"></script>';
-            $htmlContent = preg_replace('#(<\/body>)#',$bootsrapJS.'$1',$htmlContent);
+        $bootsrapJS = '<script type="text/javascript" src="'.$this->container->get('templating.helper.assets')->getUrl('bundles/skcmsadmin/js/topBar.js').'"></script>';
+        $htmlContent = preg_replace('#(<\/body>)#',$bootsrapJS.'$1',$htmlContent);
 //        }
-        
-        
-        
+
+
+
         $htmlContent = preg_replace('#(<body[ a-zA-Z0-9\"\'=_-]*>)#','$1'.$adminTopBar,$htmlContent);
-        
-        
+
+
         $response->setContent($htmlContent);
-        
-        
+
+
         return $response;
     }
-    
+
     private function getBootsrap()
     {
-        $bootrap =  '<link rel="stylesheet" href="'.$this->container->get('templating.helper.assets')->getUrl('bundles/skcmsadmin/css/topBarGrid.css').'" />';
-        $bootrap .= '<link rel="stylesheet" href="'.$this->container->get('templating.helper.assets')->getUrl('bundles/skcmsadmin/css/bootstrap-theme.min.css').'" />';
+        $bootrap ='';
+//        $bootrap =  '<link rel="stylesheet" href="'.$this->container->get('templating.helper.assets')->getUrl('bundles/skcmsadmin/css/topBarGrid.css').'" />';
+//        $bootrap .= '<link rel="stylesheet" href="'.$this->container->get('templating.helper.assets')->getUrl('bundles/skcmsadmin/css/bootstrap-theme.min.css').'" />';
         $bootrap .= '<link rel="stylesheet" href="'.$this->container->get('templating.helper.assets')->getUrl('bundles/skcmsadmin/css/font-awesome.min.css').'" />';
         $bootrap .= '<link rel="stylesheet" href="'.$this->container->get('templating.helper.assets')->getUrl('bundles/skcmsadmin/css/topBar.css').'" />';
 //        $bootrap .= '<link rel="stylesheet" href="'.$this->container->get('templating.helper.assets')->getUrl('bundles/skcmsadmin/css/zskcms.css').'" />';
-        
+
         return $bootrap;
     }
-    
+
     private function getAdminTopBar($editPath)
     {
         $siteInfo = $this->container->getParameter('skcms_admin.siteInfo');
         $entities = $this->container->getParameter('skcms_admin.entities');
         $menuGroups = $this->container->getParameter('skcms_admin.menuGroups');
         $modules = $this->container->getParameter('skcms_admin.modules');
-        
-        
+
+
         if ($modules['contact']['enabled'])
         {
             $contactParams = $modules['contact'];
@@ -85,10 +86,10 @@ class TopBarViewer
         }
 //        $this->redirect($url);
         $websitePath = $this->container->get('router')->generate($siteInfo['homeRoute']);
-        
+
         return $this->twig->render('SKCMSAdminBundle:Element/Nav:top-bar.html.twig',['websitePath'=>$websitePath,'entititesType'=>$entities,'menuGroups'=>$menuGroups,'modules'=>$modules,'newContactMessageNumber'=>$newContactMessageNumber,'editPath'=>$editPath]);
     }
-    
-    
+
+
 
 }
--- Resources/public/js/topBar.js
+++ Resources/public/js/topBar.js
@@ -1,7 +1,31 @@
 $(function(){
     skdropdowns();
-    
+    skOpener();
 });
+
+function skOpener(){
+    $('#skMenuOpener a').click(toggleSkMenu)
+}
+
+function toggleSkMenu(e){
+    if ($('#sk-topBar').hasClass('closed')){
+        $('#sk-topBar').animate({'right':'0px'},180);
+    }
+    else{
+        $('#sk-topBar').animate({'right':'-300px'},180);
+    }
+
+    $('#sk-topBar')
+        .toggleClass('closed')
+        .toggleClass('opened')
+    ;
+    $('#skMenuOpener a i')
+        .toggleClass('fa-arrow-left')
+        .toggleClass('fa-arrow-right')
+    ;
+
+
+}
 
 function skdropdowns()
 {
